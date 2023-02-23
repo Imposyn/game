b41943c27bde9223a652febdda07621ffdbf82c1
Yakup <yakup.overwatch@gmail.com>
2023-02-23 05:20:39 +0100

    before particles.js

--- final project/player.js
+++ final project/player.js
@@ -1,4 +1,4 @@
-import { Sitting, Running, Jumping, Falling } from './playerStates.js';
+import { Sitting, Running, Jumping, Falling, Rolling } from './playerStates.js';
 
 export class Player {
     constructor(game){
@@ -18,7 +18,7 @@ export class Player {
         this.frameTimer = 0;
         this.speed = 0;
         this.maxSpeed = 10;
-        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this), new Rolling(this)];
         this.currentState = this.states[0];
         this.currentState.enter();
     }
